88ccf6944fc7399961824b2c546e2dab14c948ab
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-04-16 11:56:47 +0200

    Customize article/show partial

--- apps/front/modules/article/templates/_show.php
+++ apps/front/modules/article/templates/_show.php
@@ -1,3 +1,26 @@
 <?php // Vars: $article
 
-echo $article;
+use_helper('Date');
+
+echo _open('div.clearfix');
+
+// Wrap the title in a H1
+echo _tag('h1.t_big', $article->title);
+
+// Open a P tag to display some article infos
+echo _tag('p.article_infos',
+
+  _tag('span', format_date($article->createdAt, 'D')).
+  ' | '.
+  _tag('span', $article->Author)
+);
+
+// render the article image.
+// scale it to 200px width and height.
+// give it the "image" CSS class.
+echo _media($article->Image)->size(200, 200)->set('.image');
+
+// render article body processed with markdown.
+echo markdown($article->body);
+
+echo _close('div'); 
