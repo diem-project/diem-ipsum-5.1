95dce43a120b5267f309d38b8257d241526ce0e9
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-04-16 11:17:08 +0200

    Add footer and header

--- apps/front/modules/main/templates/_footer.php
+++ apps/front/modules/main/templates/_footer.php
@@ -1,1 +1,3 @@
-<?php
+<p>
+  diem ipsum | Diem Project | website template by <a href="http://www.styleshout.com/">styleshout</a>
+</p>  
--- apps/front/modules/main/templates/_header.php
+++ apps/front/modules/main/templates/_header.php
@@ -1,1 +1,4 @@
 <?php
+echo _tag('h1.t_site', _link('@homepage')->text('diem ipsum'));
+
+echo _tag('p.t_baseline', __('meet Diem step by step')); 
