e71c58956d1cf311ac8b4ffff7df1b0ec078623c
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-04-16 11:49:53 +0200

    Setup article/list component

--- apps/front/modules/article/actions/components.class.php
+++ apps/front/modules/article/actions/components.class.php
@@ -3,9 +3,17 @@
  * Article components
  * 
  * No redirection nor database manipulation ( insert, update, delete ) here
+ * 
  */
 class articleComponents extends myFrontModuleComponents
 {
+
+  public function executeList()
+  {
+    $query = $this->getListQuery();
+    
+    $this->articlePager = $this->getPager($query);
+  }
 
 
 }
