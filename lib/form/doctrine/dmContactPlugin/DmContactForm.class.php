91cf6eb57bc5622b1eb244896ec3343f099a3552
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-04-16 11:40:19 +0200

    Setup dmContactPlugin

---
+++ lib/form/doctrine/dmContactPlugin/DmContactForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * DmContact form.
+ *
+ * @package    diem-ipsum-5.1
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id$
+ */
+class DmContactForm extends PluginDmContactForm
+{
+  public function configure()
+  {
+  }
+}
